f265811b4c6555085b1783a1db3aae5cbdd08c10
[InnCreator] <[engbethany4@gmail.com]>
2021-04-04 22:14:07 +0300

    updated child & teacher sign in css

--- bloom_learn/settings.py
+++ bloom_learn/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'm@x%dpwl@=-g2kueluz3%&_swazr0+)rq5ns&u%3$5reud$8wf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
