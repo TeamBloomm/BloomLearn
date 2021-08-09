24d9851b98a4271214fb6880940d9d9fd3ea2f88
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2021-08-09 13:30:00 +0100

    update: fixed signup form

--- js/signup/signup.js
+++ js/signup/signup.js
@@ -1,37 +1,37 @@
-let currentTab = 0; // Current tab is set to be the first tab (0)
+var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
 function showTab(n) {
-  // This function will display the specified tab of the form ...
-  let x = document.getElementsByClassName("tab");
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
-  // ... and fix the Previous/Next buttons:
+  //... and fix the Previous/Next buttons:
   if (n == 0) {
     document.getElementById("prevBtn").style.display = "none";
   } else {
     document.getElementById("prevBtn").style.display = "inline";
   }
   if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Signup";
+    document.getElementById("nextBtn").innerHTML = "Submit";
   } else {
     document.getElementById("nextBtn").innerHTML = "Next";
   }
-  // ... and run a function that displays the correct step indicator:
+  //... and run a function that will display the correct step indicator:
   fixStepIndicator(n)
 }
 
 function nextPrev(n) {
   // This function will figure out which tab to display
-  let x = document.getElementsByClassName("tab");
+  var x = document.getElementsByClassName("tab");
   // Exit the function if any field in the current tab is invalid:
   if (n == 1 && !validateForm()) return false;
   // Hide the current tab:
   x[currentTab].style.display = "none";
   // Increase or decrease the current tab by 1:
   currentTab = currentTab + n;
-  // if you have reached the end of the form... :
+  // if you have reached the end of the form...
   if (currentTab >= x.length) {
-    //...the form gets submitted:
+    // ... the form gets submitted:
     document.getElementById("regForm").submit();
     return false;
   }
@@ -50,7 +50,7 @@ function validateForm() {
     if (y[i].value == "") {
       // add an "invalid" class to the field:
       y[i].className += " invalid";
-      // and set the current valid status to false:
+      // and set the current valid status to false
       valid = false;
     }
   }
@@ -67,10 +67,6 @@ function fixStepIndicator(n) {
   for (i = 0; i < x.length; i++) {
     x[i].className = x[i].className.replace(" active", "");
   }
-  //... and adds the "active" class to the current step:
+  //... and adds the "active" class on the current step:
   x[n].className += " active";
 }
-
-
-
-
